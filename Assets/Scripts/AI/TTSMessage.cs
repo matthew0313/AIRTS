025e2a7f7d55c2adef66ac0a4e360d837bff0c28
PDLAKE-1325 <163096820+PDLAKE-1325@users.noreply.github.com>
2026-01-06 21:31:21 +0900

    TTS 기능 추가

---
+++ Assets/Scripts/AI/TTSMessage.cs
@@ -0,0 +1,42 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.Networking;
+
+public class TTSMessage : MonoBehaviour
+{
+    [SerializeField] AudioSource audioSource;
+
+    void Start()
+    {
+        if(audioSource==null)
+            audioSource = gameObject.GetComponent<AudioSource>();
+    }
+
+    public void PlayTTS(string message) => StartCoroutine(_PlayTTS(message));
+
+    IEnumerator _PlayTTS(string message)
+    {
+        string url =
+            "https://translate.google.com/translate_tts" +
+            "?ie=UTF-8" +
+            "&client=tw-ob" +
+            "&tl=ko" +
+            "&q=" + UnityWebRequest.EscapeURL(message);
+        
+        using (UnityWebRequest req =
+            UnityWebRequestMultimedia.GetAudioClip(url, AudioType.MPEG))
+        {
+            yield return req.SendWebRequest();
+
+            if (req.result != UnityWebRequest.Result.Success)
+            {
+                Debug.LogError("TTS Error: " + req.error);
+                yield break;
+            }
+
+            audioSource.clip = DownloadHandlerAudioClip.GetContent(req);
+            audioSource.Play();
+        }
+    }
+}
