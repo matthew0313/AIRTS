119fa5713a40db30df1ea013d40c258b1ffed758
matthew0313 <85425949+matthew0313@users.noreply.github.com>
2026-01-07 11:59:45 +0900

    e

--- Assets/Scripts/Entities/Player/Player.cs
+++ Assets/Scripts/Entities/Player/Player.cs
@@ -1,6 +1,5 @@
 using System;
 using System.Collections.Generic;
-using TMPro.EditorUtilities;
 using UnityEngine;
 
 [RequireComponent(typeof(CharacterController))]
--- Assets/Scripts/Entities/Player/PlayerCam.cs
+++ Assets/Scripts/Entities/Player/PlayerCam.cs
@@ -1,6 +1,5 @@
 using System;
 using System.Collections.Generic;
-using TMPro.EditorUtilities;
 using Unity.Cinemachine;
 using UnityEngine;
 
